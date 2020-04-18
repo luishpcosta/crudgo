5d41f1f529abc92e7ed1ba128eb61f3a68529bbd
Luís Henrique Paulino Costa <32212798+luishpcosta@users.noreply.github.com>
2020-04-18 17:37:16 -0300

    Update main.go

--- main.go
+++ main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"net/http"
-	"text/template"
-
 	"github.com/luis/crud/routes"
 )
-
-var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
 	routes.CarregaRotas()
